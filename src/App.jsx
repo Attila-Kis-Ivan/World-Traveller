52c66f0579af71d82936b1a6d6efbe1de8ee6ae8
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-10-10 09:45:31 +0100

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from ".//Pages/Product";
+import Pricing from ".//Pages/Pricing";
+import Home from ".//Pages/Home";
+import ErrorPage from ".//Pages/ErrorPage";
+
 const App = () => {
-  return <div>WorldTraveller</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>WordTraveler</div>;
+};
+
+export default Home;
