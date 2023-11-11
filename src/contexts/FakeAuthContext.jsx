034c070c0fe9299e2c9a24968e34ac4f9311a9f7
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-11-11 18:42:33 +0000

    implemented fake login

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -11,7 +11,6 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "login":
       return { ...state, user: action.payload, isAuthenticated: true };
-
     case "logout":
       return { ...state, user: null, isAuthenticated: false };
     default:
@@ -26,32 +25,33 @@ const FAKE_USER = {
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
 
-const AuthProvider = ({ children }) => {
+function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
-  const login = (email, password) => {
+  function login(email, password) {
     if (email === FAKE_USER.email && password === FAKE_USER.password)
       dispatch({ type: "login", payload: FAKE_USER });
-  };
+  }
 
-  const logout = () => {
+  function logout() {
     dispatch({ type: "logout" });
-  };
+  }
 
   return (
-    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
-};
+}
 
-const useAuth = () => {
+function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
-};
+  return context;
+}
 
 export { AuthProvider, useAuth };
