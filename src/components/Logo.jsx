ee7b8441d39f6220d3e572118e574a5b8135bb28
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-11-12 19:44:11 +0000

    logo and redirects

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,11 @@ import styles from "./Logo.module.css";
 const Logo = () => {
   return (
     <Link to="/">
-      <img alt="WorlTraveller logo" className={styles.logo} />
+      <img
+        src="/public/LogoWT.png"
+        alt="WorlTraveller logo"
+        className={styles.logo}
+      />
     </Link>
   );
 };
